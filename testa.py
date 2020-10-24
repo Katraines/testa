390f91fe8fe2a5530b78276af32445a84b4dae8a
Kaitlyn <rainesconjecture@gmail.com>
2020-10-23 21:03:47 -0400

    implement a cute image cleaner

--- testa.py
+++ testa.py
@@ -1,4 +1,5 @@
 # you need to install opencv 
+import cv2
 from cv2 import imread
 from cv2 import imshow
 from cv2 import imwrite
@@ -6,15 +7,29 @@ from cv2 import waitKey
 from cv2 import destroyAllWindows
 from cv2 import CascadeClassifier
 from cv2 import rectangle
+import os
 
+# first, file cleaner to remove old pictures from last time
+# specify path - can change later, I just put local /faces directory
+path = ('faces')
+
+#using listdir() method to list the files of the folder
+faces = os.listdir(path)
+
+for images in faces:
+    if images.endswith(".jpg"):
+        os.remove(os.path.join(path, images))
+
+
+# here is where detection begins 
 # load photograph
-pixels = imread('test4.jpg')
+pixels = imread('test1.jpg')
 
 # pre-trained model
 classifier = CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # perform detection
-bboxes = classifier.detectMultiScale(pixels, 1.35, 3) #configure accuracy - default settings: 1.6 & 4
+bboxes = classifier.detectMultiScale(pixels, 1.35, 3) #configure accuracy - default high settings: 1.6 & 4
 
 facex = []
 facey = []
@@ -43,17 +58,14 @@ num = 1
 for coord in facex: #count no. of facex coords collected, aka number of faces
 
 	crop_image = pixels[facey[i]:facey2[i], facex[i]:facex2[i]]
-	#imshow("Cropped image(s)", crop_image)
-	print (1)
 
+	# concat different file names for each image
 	filename = "faces/face" + str(num) + ".jpg"
-	print (filename)
 	
 	imwrite(filename, crop_image)
 
 	i += 1
 	num += 1
 
-	waitKey(0) #how to automate this?
-
+waitKey(0) #how to automate this?
 destroyAllWindows()
